67596349fc9fd218d651d68b0052bdbf1ab50376
Michel Kowal <michelkowal@hotmail.de>
2023-07-18 21:30:24 +0000

    creating new Class Channel and a function to subscribe

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 //Goal creating a website with video streming content
-
 //User can create an account and login to the website
 //User can create an own channel and subscribe to other channels
 //User can upload a video and it will be streamed to the website
@@ -7,27 +6,57 @@
 //User can comment and like a video
 //User can search for a video
 //User can see the most popular videos
-class Users {
+class User {
   constructor(name, email, age, newUser) {
     this.name = name
     this.email = email
     this.age = age
     this.newUser = newUser
   }
-  checkNewUser(name) {
-    if (this.newUser) {
-      console.log(`You still have an account, ${this.name} please
-    login to your account`)
+  checkNewUser = () => {
+    if (this.newUser === true) {
+      console.log('Welcome to our website ' + this.name)
+      return (this.newUser = false)
     } else {
-      console.log(`Welcome You have created a new account, ${this.name}`)
+      console.log('Welcome back ' + this.name)
     }
   }
 }
-const user0 = new Users('Mitch', 'mitch@web.de', 36, false)
+const user0 = new User('Mitch', 'mitch@web.de', 36, false)
 user0.checkNewUser(this.name)
 
-const user1 = new Users('John', 'john@web.de', 17, true)
+const user1 = new User('John', 'john@web.de', 17, true)
 user1.checkNewUser(this.name)
 
-const user2 = new Users('Peter', 'pete@web.de', 19, false)
+const user2 = new User('Peter', 'pete@web.de', 19, false)
 user2.checkNewUser(this.name)
+
+console.log(user0)
+
+console.log('-------------------------------------------------------------')
+
+//User can create an own channel
+class Channel {
+  subscribers = []
+
+  constructor(name, channelName) {
+    this.name = name
+    this.channelName = channelName
+  }
+  subscribe = name => {
+    return this.subscribers.push(name)
+  }
+}
+// user creates channel
+const channel0 = new Channel(user0.name, 'Mitchs Channel')
+const channel1 = new Channel(user1.name, 'Johns Channel')
+const channel2 = new Channel(user2.name, 'Peters Channel')
+
+console.log(channel0)
+
+//function that User can subscribe to other users channels
+
+//Users can subscribe to other channels
+channel0.subscribe(user1.name)
+channel0.subscribe(user2.name)
+console.log(channel0)
